4c67a5a2b2b42792db8284ff0a3602c807c1a6bd
branseb@gmail.com <branseb@gmail.com>
2022-04-06 18:08:30 +0200

    opakovanie

---
+++ cvicenie/Date.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cvicenie
+{
+    internal class Date
+
+
+    {
+        public static void Now()
+
+        {
+            var dayofweek = DateTime.Now.DayOfWeek;
+            var day = DateTime.Now.Day;
+            var month = DateTime.Now.Month -1;
+            var year = DateTime.Now.Year;
+            var hour = DateTime.Now.Hour;
+            var minute = DateTime.Now.Minute;
+
+            Console.WriteLine(dayofweek);
+            Console.WriteLine(day + "." + month + "." + year);
+            Console.WriteLine(hour + ":" + minute);
+
+
+        }
+    }
+}
--- cvicenie/Program.cs
+++ cvicenie/Program.cs
@@ -10,8 +10,26 @@ namespace cvicenie
     {
         static void Main(string[] args)
         {
+            Date.Now();
+            while (true)
+            {
+                Console.WriteLine("zadaj prvy vek");
+
+                var vek1 = Console.ReadLine();
+                Console.WriteLine("zadaj druhy vek");
+                var vek2 = Console.ReadLine();
+                int cislo1 = Convert.ToInt32(vek1);
+                int cislo2 = Convert.ToInt32(vek2);
+
+                int vysledok = Age.Calculate(cislo1, cislo2);
+                Console.WriteLine(vysledok);
+            }
             
-            Console.WriteLine("ahoj");
+            Console.ReadLine();
+
+
+
+            /*Console.WriteLine("ahoj");
             Console.WriteLine("Zadaj prvy vek");
             string vek1 = Console.ReadLine();
             Console.WriteLine("Zadaj druhy vek");
@@ -24,6 +42,7 @@ namespace cvicenie
 
 
             Console.ReadLine();
+            */
 
 
 
