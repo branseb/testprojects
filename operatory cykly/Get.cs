464b298b1f2ca706dd8d7f9e59c4d4ecf0a86e07
branseb@gmail.com <branseb@gmail.com>
2022-04-07 21:52:11 +0200

    if else

---
+++ operatory cykly/Get.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operatory_cykly
+{
+    internal class Cisla
+    {
+        public static string Getnumbername(string cislo)
+        {
+
+            if (cislo == "1")
+            {
+                return  "jeden";
+            }
+            else if (cislo == "2")
+            {
+                return ("dva");
+            }
+            else if (cislo == "3")
+            {
+                return ("tri");
+            }
+            else if (cislo == "4")
+            {
+                return ("štyri");
+            }
+            else if (cislo == "5")
+            {
+                return ("päť");
+            }
+            else if (cislo == "6")
+            {
+                return ("šesť");
+            }
+            else if (cislo == "7")
+            {
+                return ("sedem");
+            }
+            else if (cislo == "8")
+            {
+                return ("osem");
+            }
+            else if (cislo == "9")
+            {
+                return ("deväť");
+            }
+            else
+            {
+                return ("Neplatné číslo!!!");
+            }
+        }
+    }
+}
---
+++ operatory cykly/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace operatory_cykly
+{ internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //while (true)
+             
+
+            {
+                /*  Console.WriteLine("Zadaj cislo od 1 do 9:");
+                  string cislo = Console.ReadLine();
+
+                  if (cislo == "1")
+                  {
+                      Console.WriteLine("jeden");
+                  }
+                  else if (cislo == "2")
+                  {
+                      Console.WriteLine("dva");
+                  }
+                  else if (cislo == "3")
+                  {
+                      Console.WriteLine("tri");
+                  }
+                  else if (cislo == "4")
+                  {
+                      Console.WriteLine("štyri");
+                  }
+                  else if (cislo == "5")
+                  {
+                      Console.WriteLine("päť");
+                  }
+                  else if (cislo == "6")
+                  {
+                      Console.WriteLine("šesť");
+                  }
+                  else if (cislo == "7")
+                  {
+                      Console.WriteLine("sedem");
+                  }
+                  else if (cislo == "8")
+                  {
+                      Console.WriteLine("osem");
+                  }
+                  else if (cislo == "9")
+                  {
+                      Console.WriteLine("deväť");
+                  }
+                  else
+                  {
+                      Console.WriteLine("Neplatné číslo!!!");
+                  }
+                 */
+
+            }
+            Console.WriteLine("Napis cislo od 1 o 9 :");
+            string cislo = Console.ReadLine();
+           string slovo= Cisla.Getnumbername(cislo);
+            Console.WriteLine(slovo);
+
+            Console.ReadLine();
+        }
+            }
+}
