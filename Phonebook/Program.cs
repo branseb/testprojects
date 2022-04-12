1455d8836c999e76ba859d9b9788b8f82c653e1c
branseb@gmail.com <branseb@gmail.com>
2022-04-12 16:49:17 +0200

    oprava menicky {zaokruhlovanie} telefonny zoznam

---
+++ Phonebook/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Phonebook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string phonebook = "phonebook.txt";
+            File.AppendAllText(phonebook,"");
+            Console.WriteLine("Phonebook"+Environment.NewLine+"Press \"n\" for New contact or \"o\" for open Phonebook");
+            while (true)
+            {
+               
+                char select = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                string[] books = File.ReadAllLines(phonebook);
+                var fltere = books.Where(row => row.Contains("er"));
+                if (select == 'n')
+                {
+                    Console.WriteLine("Whrite The Name:");
+                    string newcontname = Console.ReadLine();
+                    Console.WriteLine("Number on {0} ?", newcontname);
+                    string newcontnum = Console.ReadLine();
+                    File.AppendAllText(phonebook, newcontname + " - " + newcontnum + Environment.NewLine);
+                    Console.WriteLine("Saved!");
+                }
+                else if (select == 'o')
+                {
+                    Console.Write("Contacts:" + Environment.NewLine + string.Join(Environment.NewLine, fltere)); 
+                    
+                }
+
+                else
+                {
+                    Console.WriteLine("Wrong choice!");
+                }
+            }
+        }
+    }
+}
--- ticket machine/Program.cs
+++ ticket machine/Program.cs
@@ -45,7 +45,7 @@
 
 
             returned = insertodouble - ticketprince;
-            if (returned < 0)
+            if (returned < 0.00)
             {
                 convert = true;
                 Console.WriteLine("Little money! \nInsert coin!");
@@ -56,8 +56,33 @@
                 Console.WriteLine("Back: " + returned + "$");
                 convert = false;
             }
+
         }
-        double zvysok2 = returned % 2;
+        //int return2 =(int) returned/2;
+        //int return1 = (int)((returned - return2*2)/1);
+        //int return05= (int)((returned - return2*2-return1)/0.5);
+        //int return02 = (int)((returned - return2*2 - return1 - return05*0.5)/0.2);
+        //int return01 = (int)((returned - return2*2 - return1 - return05*0.5 - return02*0.2)/0.1);
+        //int return005 = (int)((returned - return2*2 - return1 - return05*0.5 - return02*0.2 - return01*0.1)/0.05);
+        //int return002 = (int)((returned - return2*2 - return1 - return05*0.5 - return02*0.2 - return01*0.1 - return005*0.05)/0.02);
+        //int return001 = (int)((returned - return2*2 - return1 - return05*0.5 - return02*0.2 - return01*0.1 - return005*0.05 -return002*0.02)/0.01);
+
+        int eur2 = (int)returned / 2;
+        returned = Math.Round(returned - eur2 * 2, 2);
+        int eur1 = (int)returned / 1;
+        returned = Math.Round(returned - eur1 * 1, 2);
+        int eur05 = (int)(returned / 0.5);
+        returned = Math.Round(returned - eur05 * 0.5, 2);
+        int eur02 = (int)(returned / 0.2);
+        returned = Math.Round(returned - eur02 * 0.2, 2);
+        int eur01 = (int)(returned / 0.1);
+        returned = Math.Round(returned - eur01 * 0.1, 2);
+        int eur005 = (int)(returned / 0.05);
+        returned = Math.Round(returned - eur005 * 0.05, 2);
+        int eur002 = (int)(returned / 0.02);
+        returned = Math.Round(returned - eur002 * 0.02, 2);
+    
+        /*double zvysok2 = returned % 2;
         int return2 = (int)(returned - zvysok2) / 2;
         double zvysok1 = zvysok2 % 1;
         int return1 = (int)((zvysok2 - zvysok1) / 1);
@@ -72,11 +97,16 @@
         double zvysok002 = zvysok005 % 0.02;
         int return002 = (int)((zvysok005 - zvysok002) / 0.02);
         int return001 = (int)(zvysok002 / 0.01);
-
-        Console.WriteLine("returned \n2$ - {0}pcs \n1$ - {1}pcs \n0.5$ - {2}pcs \n0.2$ - {3}pcs \n0.1$ - {4}pcs \n0.05$ - {5}pcs \n0.02$ - {6}pcs \n0.01$ - {7}pcs", return2, return1, return05, return02, return01, return005, return002, return001);
-
+        */
 
 
+       //Console.WriteLine("returned \n2$ - {0}pcs \n1$ - {1}pcs \n0.5$ - {2}pcs \n0.2$ - {3}pcs \n0.1$ - {4}pcs \n0.05$ - {5}pcs \n0.02$ - {6}pcs \n0.01$ - {7}pcs", return2, return1, return05, return02, return01, return005, return002, return001);
+       Console.WriteLine("returned \n2$ - {0}pcs \n1$ - {1}pcs \n0.5$ - {2}pcs \n0.2$ - {3}pcs \n0.1$ - {4}pcs \n0.05$ - {5}pcs \n0.02$ - {6}pcs \n0.01$ - {7}pcs", eur2, eur1, eur05, eur02, eur01, eur005, eur002, returned);
+
+
+        File.AppendAllText("cisla.txt","erik 5455 \n");
+        string text = File.ReadAllText("cisla.txt");
+        Console.WriteLine(text);
 
         Console.ReadLine();
     }
